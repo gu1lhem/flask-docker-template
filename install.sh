bdb20edbfcd2982d64992489e96aa0f893382ffe
gu1lhem <38001093@parisnanterre.fr>
2022-01-29 14:54:31 +0100

    Enhanced install script.

--- install.sh
+++ install.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-key_file="id_rsa"
+# Pour le dépôt, il faut créer une paire de clés (voir https://github.com/appleboy/ssh-action)
+# Et exécuter 
+ssh "example.com" ssh-keygen -l -f /etc/ssh/ssh_host_ed25519_key.pub | cut -d ' ' -f2
+# sur la machine pour obtenir la fingerprint (mettre tout le résultat de la commande comme secret).
+# puis ajouter la clé publique au fichier .ssh/authorized_keys.
+
+key_file="flask-docker-test"
 
 # Install Docker and Nginx
-apt update
+sudo apt update
+sudo apt dist-upgrade -y
+sudo apt autoremove -y
 sudo apt install docker.io docker-compose nginx -y
 
 # Create usergroup for Docker to be executed without root
 sudo usermod -a -G docker $USER
 newgrp docker
-#? Il faut peut-être se déconnecter puis reconnecter ici.
 
 # Clés SSH pour GitHub
-ssh-keygen
+ssh-keygen -f .ssh/$key_file -N ""
 eval "$(ssh-agent -s)"
 # Ne pas spécifier de mot de passe
 cat .ssh/$key_file.pub
@@ -43,14 +50,17 @@ sudo nginx -s reload
 # Avec docker-compose :
 docker-compose up
 
-# Pour installer un service systemd, lancer sudo chmod +x ./systemd.sh && sudo ./systemd.sh
+# Pour installer un service systemd :
+chmod +x ./systemd.sh
+sudo ./systemd.sh
 
 
 # Autoriser l'utilisateur à exécuer la commande restart pour ce service.
-echo "Copiez ceci dans `sudo visudo` pour permettre le redémarrage automatique du service."
-echo $(whoami)" ALL=NOPASSWD /bin/systemctl restart "$(basename $(pwd))".service"
+echo "Copiez ceci dans sudo visudo pour permettre le redémarrage automatique du service."
+echo $(whoami)" ALL=NOPASSWD:/bin/systemctl restart "$(basename $(pwd))".service"
 
 # From :
+# https://github.com/appleboy/ssh-action
 # https://blog.miguelgrinberg.com/post/how-to-dockerize-a-react-flask-project
 # https://www.toptal.com/flask/flask-production-recipes
 # https://techoverflow.net/2020/10/24/create-a-systemd-service-for-your-docker-compose-project-in-10-seconds/
